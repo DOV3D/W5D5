7c3b3f0fe434347a3c7a048d30f1561de7250db0
DOV3D <davidovelazquez3@gmail.com>
2021-09-10 15:10:25 -0400

    Add eighties movies method

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -2,12 +2,14 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989)
+        .where(score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  # Movie.where("score < 8.0").distinct.pluck(:yr)
+  Movie.where.not(yr: "score > 8.0").pluck(:yr)
 end
 
 def cast_list(title)
