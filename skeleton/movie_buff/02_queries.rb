5f97481e8aa8df77b0ebaa72ea0a39df4b25e97a
Tristan Angeles <setswipe@gmail.com>
2021-09-10 15:47:10 -0400

    Working on cast_list

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -16,7 +16,9 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+
+  Actor.select(:id, :name).joins(:movies)
+      .where(movie: title).order(castings: {casting.ord} ASC)
 
 end
 
